983a4a3f9bef21ae92f223b5b7edd81124b71054
Natalia Mellado <nataliamelladocani@gmail.com>
2024-01-26 16:24:50 -0300

    Agrega eventos y manejadores en 'script.js'.

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// Configuración de tooltip para el botón "enviar" y para los íconos de redes sociales //
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// Configuración de popover para el logo 'Viajes Chile' en el footer//
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Manejador de evento para el botón "enviar" //
+$(document).ready(function () {
+    $("#enviarMensaje").click(function () {
+        alert("El mensaje fue enviado correctamente.");
+    });
+});
+
+// Smooth Scroll Inicio //
+$(document).ready(function() {
+    $('a.nav-link[href="#inicio"]').on('click', function(event) {
+      event.preventDefault();
+      var targetPosition = $('#inicio').offset().top;
+
+      $('html, body').animate({
+        scrollTop: targetPosition
+      }, 800); 
+    });
+  });
+
+
+// Smooth Scroll About-section //
+  $(document).ready(function() {
+    $('a.nav-link[href="#quienes-somos"]').on('click', function(event) {
+      event.preventDefault();
+      var targetPosition = $('#quienes-somos').offset().top;
+
+      $('html, body').animate({
+        scrollTop: targetPosition
+      }, 800); 
+    });
+  });
+
+  // Smooth Scroll Featured-section //
+  $(document).ready(function() {
+    $('a.nav-link[href="#destacados"]').on('click', function(event) {
+      event.preventDefault();
+      var targetPosition = $('#destacados').offset().top;
+
+      $('html, body').animate({
+        scrollTop: targetPosition
+      }, 800); 
+    });
+  });
+
+    // Smooth Scroll Featured-section //
+    $(document).ready(function() {
+        $('a.nav-link[href="#contacto"]').on('click', function(event) {
+          event.preventDefault();
+          var targetPosition = $('#contacto').offset().top;
+    
+          $('html, body').animate({
+            scrollTop: targetPosition
+          }, 800); 
+        });
+      });
+
